70430f625c5712e21af5a387494aaad7c6d0d5af
federicotempo <federicotempo98@gmail.com>
2024-10-19 11:34:33 -0300

    create main function and createDivs function
    This commit creates the main function and the createDivs function. Additionally, it creates the 'divs' class to manipulate the shape of the square divs.

---
+++ main.js
@@ -0,0 +1,17 @@
+
+function createDivs() {
+    const container = document.querySelector("#container");
+
+    for (let i = 0; i < 256; i++) {
+        const newDiv = document.createElement("div");
+        newDiv.classList.add("divs");
+        container.appendChild(newDiv);
+    }
+
+}
+
+function main() {
+    createDivs();
+}
+
+main();
