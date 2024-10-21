56deb9d1ab83f1f5474a75c528bee023e0258a11
federicotempo <federicotempo98@gmail.com>
2024-10-21 17:29:13 -0300

    Create changeDivColor
    It creates a new function to change the color of the div when clicking on it.

--- main.js
+++ main.js
@@ -5,8 +5,13 @@ function createDivs() {
     for (let i = 0; i < 256; i++) {
         const newDiv = document.createElement("div");
         newDiv.classList.add("divs");
+        newDiv.addEventListener("click", () => changeDivColor(newDiv));
         container.appendChild(newDiv);
     }
+}
+
+function changeDivColor(div) {
+    div.style.backgroundColor = "gray";
 }
 
 function main() {
