b67d6ccb403d63be5cf3cad00f02104a4bcd3d03
federicotempo <federicotempo98@gmail.com>
2024-10-21 17:50:24 -0300

    Modify changeDivColor
    This commit modifies changeDivColor for better functionality.

--- main.js
+++ main.js
@@ -5,17 +5,26 @@ function createDivs() {
     for (let i = 0; i < 256; i++) {
         const newDiv = document.createElement("div");
         newDiv.classList.add("divs");
+        newDiv.style.backgroundColor = originalColor;
         newDiv.addEventListener("click", () => changeDivColor(newDiv));
         container.appendChild(newDiv);
     }
 }
 
 function changeDivColor(div) {
-    div.style.backgroundColor = "gray";
+    const newColor = "gray";
+
+    if (div.style.backgroundColor === newColor) {
+        div.style.backgroundColor = originalColor;
+    } else {
+        div.style.backgroundColor =newColor;
+    }
 }
 
 function main() {
     createDivs();
 }
+
+const originalColor = "white";
 
 main();
